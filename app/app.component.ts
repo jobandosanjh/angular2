c19fc427138ba87a97a1f6bb83d6143812c71ac9
Jobanpreet Singh <jobandosanjh@ymail.com>
2017-06-07 11:59:48 +0530

    Validation Logic

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello World</h1>'
+  templateUrl: 'app/app.model.driven.form.component.html',
+  styleUrls: ['app/app.model.driven.form.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+
+  private defaultCity:string = "Gurugram";
+  private userForm : FormGroup;
+
+  constructor(private _formBuilder: FormBuilder){}
+
+  public ngOnInit(){
+    this.userForm = this._formBuilder.group({
+      name:['name', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+      email:['email@email.com'],
+      address: this._formBuilder.group({
+        street: ['street'],
+        city: [this.defaultCity],
+        postalCode: [null, Validators.pattern('^[0-9]{5}$')]
+      })
+    })
+  }
+
+  private onSubmit(value:any){
+    console.log(value);
+  }
+
+  private onFormSubmit(){
+    console.log(this.userForm.value);
+  }
+}
